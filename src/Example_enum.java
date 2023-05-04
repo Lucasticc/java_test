8616fd9272608d0faaabb9eb3bea8012cd4c8bdb
lucas <lucasprint@163.com>
2023-05-04 21:00:47 +0800

    Changes

--- src/Example_enum.java
+++ src/Example_enum.java
@@ -4,6 +4,7 @@ class FreshJuice {
     FreshJuiceSize size;
 }
 //好像就是给定预先设置好的值
+//在类中生命 属性和方法 对对象进行操作 类型变量
 public class Example_enum {
     public static void main(String[] args){
         FreshJuice juice = new FreshJuice();
--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,12 @@
 public class Hello {
     // static关键字：静态 void 返回类型 main方法名
+    //构造器
+    public Hello(String name){
+        System.out.println(name);
+    }
     public static void main(String[] args){
         System.out.println("hello");
+        Hello my = new Hello("s");
+        //是不是属于调用构造器 构造器里已经完成
     }
 }
